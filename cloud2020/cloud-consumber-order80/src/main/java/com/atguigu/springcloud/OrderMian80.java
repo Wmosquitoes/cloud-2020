27815173eebd5f4cb04a22d1faa9d1dc66738a06
sfw <190956132@qq.com>
2020-04-22 16:56:52 +0800

    Eureka 将消费端和提供端都注册到   注册中心

--- cloud2020/cloud-consumber-order80/src/main/java/com/atguigu/springcloud/OrderMian80.java
+++ cloud2020/cloud-consumber-order80/src/main/java/com/atguigu/springcloud/OrderMian80.java
@@ -3,8 +3,10 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class OrderMian80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMian80.class,args);
--- cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8081.java
+++ cloud2020/cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/PaymentMain8081.java
@@ -2,8 +2,10 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class PaymentMain8081 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8081.class,args);
